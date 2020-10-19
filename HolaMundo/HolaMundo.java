ce4c76c7a1c514cf35eac5bcaebc7c78165a77ec
Pedro Ugalde <pugalded1900@alumno.ipn.mx>
2020-10-19 12:26:46 -0500

    Ejemplo de HolaMundo

---
+++ HolaMundo/HolaMundo.java
@@ -0,0 +1,40 @@
+//asi es como podemos comentar una línea es un comentario doble diagonal
+
+/*
+diagonal mas signo de multiplicas
+comentar varias lineas
+a
+la
+vez 
+y
+escribir 
+
+mas 
+de
+una
+linea de comentario
+
+*/
+
+class HolaMundo{
+    //aqui va a estar el codigo de programación
+    //es el cuerpo del programa
+
+    //clase principal
+
+    public static void main (String[ ] args){
+        /*este es el metodo principal de cualquier clase de java
+
+        en el cual se especifivca que va a poder recibir cadenas y argumentos
+
+        los argumentos son cualquier tipo de valor que pueden recibir este metodo principal
+
+        aqui se especifica el cuerpo del algoritmo
+        */
+
+        System.out.print("Hola Mundo, ya no eres kawaii");
+
+        //esto es lo mismo que:
+        // Escribir "Hola mundo" en pseintja
+    }
+}
---
+++ Wodofoc/wodofoc.java
@@ -0,0 +1,11 @@
+
+
+public class wodofoc{
+    public static void main (String [] args) {
+
+        System.out.printf("Hello %s!%n", "World");
+        System.out.printf("Hello !", "World");
+
+    }
+
+}
